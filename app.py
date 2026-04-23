118f782053848f71bd5740b117ec6159241f10e6
WellingtonLCR <dabliojau@gmail.com>
2026-04-23 20:29:33 -0300

    updt

--- app.py
+++ app.py
@@ -7,18 +7,17 @@ app.secret_key = 'imc_secret_key_2026'
 
 @app.route('/')
 def index():
-    sql = '''
-CREATE TABLE IF NOT EXISTIS calculos(
+    sql = sql = """
+CREATE TABLE IF NOT EXISTS calculos (
     id_calculos BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    peso  DECIMAL(6,2) NOT NULL,
+    peso DECIMAL(6,2) NOT NULL,
     altura DECIMAL(5,2) NOT NULL,
-
     criado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    alterado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    alterado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deletado_em DATETIME NULL
 );
-'''
+"""
     resultados = execute_query(sql, fetch=True)
     print(resultados)
     
@@ -34,11 +33,21 @@ def resultados():
 @app.route('/calcular', methods=['GET', 'POST'])
 def calcular():
     if request.method == 'POST':
-        nome = request.form.get('nome', 'Não foi enviado um nome')
-        peso = request.form.get('peso')
-        altura = request.form.get('altura')
+        nome = request.form.get('nome', 'Não foi enviado um nome').strip()
+        peso = request.form.get('peso').strip()
+        altura = request.form.get('altura').strip()
 
         try:
+            sql = 'INSERT INTO calculos(nome, peso, altura) VALUES(%s, %s, %s);'
+            execute_query(sql, nome, peso, altura)
+            flash(f'Produto[{nome}] cadastrado com sucesso', 'success')
+            return redirect(url_for('resultados'))
+            
+        except Exception as e:
+            flash(f'Erro ao salvar', 'danger')
+            app.logger.error(f'Erro no INSERT:{e}')
+            return redirect(url_for(calcular))
+            
             peso = float(peso)
             altura = float(altura)
 
