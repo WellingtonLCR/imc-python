5e9f683d2b10bee7db047da674bc52c03cfff514
WellingtonLCR <dabliojau@gmail.com>
2026-04-23 19:37:13 -0300

    corrigido bug no bd

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ DB_PARAMS = {
     'password': '',
     'database': 'bd_imc',
     'charset': 'utf8mb4',
-    'timezone': '-03:00',
+    'time_zone': '-03:00',
     'use_pure': True,
     'connect_timeout': 10
 }
